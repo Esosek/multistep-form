f7668047f8f73aa95325f58509eea8e2fdd1396f
Ales Zima <ales@cpex.cz>
2024-05-06 07:47:18 +0200

    Added Layout with bg image

---
+++ src/components/App.svelte
@@ -0,0 +1,7 @@
+<script>
+  import FormLayout from '../layouts/FormLayout.svelte';
+</script>
+
+<FormLayout activeStep={0}>
+  <h1>Personal info</h1>
+</FormLayout>
---
+++ src/layouts/FormLayout.svelte
@@ -0,0 +1,24 @@
+<script>
+  export let activeStep = 0;
+</script>
+
+<body>
+  <main class="min-h-lvh">
+    <div>
+      <slot />
+    </div>
+  </main>
+</body>
+
+<style>
+  main {
+    background-image: url('/assets/images/bg-sidebar-mobile.svg');
+    background-repeat: no-repeat;
+  }
+
+  @media only screen and (min-width: 600px) {
+    main {
+      background-image: url('/assets/images/bg-sidebar-desktop.svg');
+    }
+  }
+</style>

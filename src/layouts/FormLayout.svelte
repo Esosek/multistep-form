7d87337ed7723c6255b58ed3ab9ffd347503f6f3
Ales Zima <ales@cpex.cz>
2024-05-07 11:40:41 +0200

    Added mobile FormLayout

--- src/layouts/FormLayout.svelte
+++ src/layouts/FormLayout.svelte
@@ -3,24 +3,31 @@
   export let activeStep = 0;
 </script>
 
-<body class="text-marine-blue">
-  <main class="min-h-lvh flex flex-col items-center">
-    <ul class="flex gap-4 my-4">
+<body class="text-marine-blue bg-magnolia min-h-lvh">
+  <main class="flex flex-col items-center">
+    <ul class="flex gap-4 my-8">
       {#each { length: stepCount } as _, index}
         <li>
           <button
             type="button"
-            class="size-10 aspect-square rounded-full border-[1px] text-white aria-[current='true']:bg-pastel-blue aria-[current='true']:text-inherit"
+            class="size-10 aspect-square rounded-full border-[1px] text-white font-medium aria-[current='true']:bg-light-blue aria-[current='true']:text-inherit"
             aria-current={index === activeStep ? 'true' : ''}
             >{index + 1}</button
           >
         </li>
       {/each}
     </ul>
-    <div>
+    <div class="bg-white rounded-lg w-[90%] mx-4 p-8 shadow-lg">
       <slot />
     </div>
   </main>
+  <div
+    class="fixed bottom-0 left-0 right-0 bg-white p-4 shadow-[0_-4px_6px_-1px_rgba(0,0,0,0.1 )]"
+  >
+    <button class="block bg-marine-blue text-white ml-auto py-2 px-4 rounded-md"
+      >Next Step</button
+    >
+  </div>
 </body>
 
 <style>

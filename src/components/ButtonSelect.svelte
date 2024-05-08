6e8a45edfdfd9cec6b8e5115c90009a62504eef3
Ales Zima <ales@cpex.cz>
2024-05-08 20:28:20 +0200

    Added plan button select

---
+++ src/components/ButtonSelect.svelte
@@ -0,0 +1,22 @@
+<script>
+  export let iconPath;
+  export let label = 'Label';
+  export let subtext;
+  export let isSelected = false;
+  export let onPress = () => {};
+</script>
+
+<button
+  on:click={onPress}
+  class={`flex items-center gap-4 w-full border-[1px]
+  p-4 rounded-lg sm:flex-col sm:items-start sm:gap-12 ${isSelected ? 'border-purplish-blue bg-magnolia' : 'border-light-gray'}`}
+>
+  {#if iconPath}
+    <img src={iconPath} alt="" />
+  {/if}
+
+  <div class="flex flex-col text-left">
+    <p class="font-bold">{label}</p>
+    <p class="text-sm text-cool-gray">{subtext}</p>
+  </div>
+</button>
--- src/components/FormStep02.svelte
+++ src/components/FormStep02.svelte
@@ -3,6 +3,31 @@
   import FormNavigation from './FormNavigation.svelte';
   import FormButton from './FormButton.svelte';
   import formStepCounter from '../stores/formStepCounter';
+  import ButtonSelect from './ButtonSelect.svelte';
+
+  const plans = [
+    {
+      label: 'Arcade',
+      price: '$9/mo',
+      icon: '/assets/images/icon-arcade.svg',
+    },
+    {
+      label: 'Advanced',
+      price: '$12/mo',
+      icon: '/assets/images/icon-advanced.svg',
+    },
+    {
+      label: 'Pro',
+      price: '$15/mo',
+      icon: '/assets/images/icon-pro.svg',
+    },
+  ];
+
+  let selectedPlanIndex = 0;
+
+  function selectPlan(index) {
+    selectedPlanIndex = index;
+  }
 </script>
 
 <div class="sm:flex sm:flex-col sm:h-full">
@@ -10,6 +35,19 @@
     title="Select your plan"
     subtext="You have the option of monthly of yearly billing."
   />
+  <div class="grid gap-3 sm:grid-cols-3">
+    {#each plans as plan, index}
+      <ButtonSelect
+        label={plan.label}
+        subtext={plan.price}
+        iconPath={plan.icon}
+        isSelected={index === selectedPlanIndex}
+        onPress={() => selectPlan(index)}
+      />
+    {/each}
+  </div>
+
+  <div id="toggle"></div>
   <FormNavigation>
     <FormButton
       label="Go Back"

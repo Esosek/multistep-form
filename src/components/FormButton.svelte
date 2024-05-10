8915e2a8355d0c4792c4909a744e9081a091367e
Ales Zima <ales@cpex.cz>
2024-05-10 16:39:37 +0200

    Added bgColor param FormButton

--- src/components/FormButton.svelte
+++ src/components/FormButton.svelte
@@ -2,10 +2,10 @@
   export let label = 'Next Step';
   export let type = 'button';
   export let isPrimary = true;
+  export let bgColor = 'bg-marine-blue';
   export let onPress = () => {};
 
-  let classes =
-    'bg-marine-blue text-white rounded-md py-2 px-4 sm:px-6 hover:bg-marine-blue/85';
+  let classes = `${bgColor} text-white rounded-md py-2 px-4 sm:px-6 hover:bg-marine-blue/85`;
 
   if (!isPrimary) {
     classes = 'text-cool-gray hover:text-marine-blue';

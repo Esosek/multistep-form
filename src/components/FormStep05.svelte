f6ce5bd7a837a56e46f307b5f853d8da1dc5cedc
Ales Zima <ales@cpex.cz>
2024-05-13 17:13:49 +0200

    Added mobile step 5

--- src/components/FormStep05.svelte
+++ src/components/FormStep05.svelte
@@ -1,8 +1,10 @@
-<div class="flex flex-col h-full items-center justify-center gap-4 px-10">
+<div
+  class="flex flex-col h-full items-center justify-center gap-4 py-10 px-1 leading-6 sm:px-10"
+>
   <img
     src="/assets/images/icon-thank-you.svg"
     alt="Thanks icon"
-    class="max-h-[80px] max-w-[80px] my-4"
+    class="max-h-16 sm:max-h-[80px] my-4"
   />
   <h1 class="text-marine-blue text-2xl font-bold flex-grow-0">Thank you!</h1>
   <p class="text-cool-gray text-center">
